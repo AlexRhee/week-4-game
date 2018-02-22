dafa17db377ee895015c04e4bf295510c21c3cf2
arhee <arheeee@gmail.com>
2018-02-21 22:50:02 -0800

    working copy, values now equate correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,48 +21,130 @@ $("#crystal1").on("click", function () {
     numberTotal += crystal1;
     console.log(numberTotal);
     $("#total").html(numberTotal);
+    if (numberTotal == Number(rando)) {
+        alert("YOU WIN!!!");
+        wins++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#wins").html(wins);
+        console.log("win");
+    }
+    else if (numberTotal > Number(rando)) {
+        alert("YOU LOSE!!!");
+        losses++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#losses").html(losses);
+        console.log("lose");
+    }
 });
 
 $("#crystal2").on("click", function () {
     numberTotal += crystal2;
     console.log(numberTotal);
     $("#total").html(numberTotal);
+    if (numberTotal == Number(rando)) {
+        alert("YOU WIN!!!");
+        wins++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#wins").html(wins);
+        console.log("win");
+    }
+    else if (numberTotal > Number(rando)) {
+        alert("YOU LOSE!!!");
+        losses++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#losses").html(losses);
+        console.log("lose");
+    }
 });
 
 $("#crystal3").on("click", function () {
     numberTotal += crystal3;
     console.log(numberTotal);
     $("#total").html(numberTotal);
+    if (numberTotal == Number(rando)) {
+        alert("YOU WIN!!!");
+        wins++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#wins").html(wins);
+        console.log("win");
+    }
+    else if (numberTotal > Number(rando)) {
+        alert("YOU LOSE!!!");
+        losses++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#losses").html(losses);
+        console.log("lose");
+    }
 });
 
 $("#crystal4").on("click", function () {
     numberTotal += crystal4;
     console.log(numberTotal);
     $("#total").html(numberTotal);
+    if (numberTotal == Number(rando)) {
+        alert("YOU WIN!!!");
+        wins++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#wins").html(wins);
+        console.log("win");
+    }
+    else if (numberTotal > Number(rando)) {
+        alert("YOU LOSE!!!");
+        losses++;
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+        rando = ((Math.floor(Math.random() * 102)) + 19);
+        $("#score").text(rando);
+        numberTotal = 0;
+        $("#losses").html(losses);
+        console.log("lose");
+    }
 });
 
 
 
 
-if (numberTotal == Number(rando)) {
-    alert("YOU WIN!!!");
-    wins++;
-    crystal1 = (Math.floor(Math.random() * 12) + 1);
-    crystal2 = (Math.floor(Math.random() * 12) + 1);
-    crystal3 = (Math.floor(Math.random() * 12) + 1);
-    crystal4 = (Math.floor(Math.random() * 12) + 1);
-    numberTotal = 0;
-    $("#wins").html(wins);
-    console.log("win");
-}
-else if (numberTotal > Number(rando)) {
-    alert("YOU LOSE!!!");
-    losses++;
-    crystal1 = (Math.floor(Math.random() * 12) + 1);
-    crystal2 = (Math.floor(Math.random() * 12) + 1);
-    crystal3 = (Math.floor(Math.random() * 12) + 1);
-    crystal4 = (Math.floor(Math.random() * 12) + 1);
-    numberTotal = 0;
-    $("#losses").html(losses);
-    console.log("lose");
-}
